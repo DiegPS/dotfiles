5fb3942fc46740dcfab57616c464ac9cc52a46c6
Diego Pahua Silvan <88301232+DiegPS@users.noreply.github.com>
2024-04-11 21:03:35 -0600

    lsd tree add windows

--- windows/clink/clink_start.cmd
+++ windows/clink/clink_start.cmd
@@ -3,10 +3,16 @@
 doskey e=explorer "%%cd%%"
 doskey cat=bat --paging=never -p $*
 
+doskey edge=start msedge
+
+doskey neofetch=wsl neofetch
+
 doskey l=lsd --git --group-directories-first $*
 doskey ls=lsd --git --group-directories-first $*
 doskey ll=lsd --git --group-directories-first -l $*
 doskey la=lsd --git --group-directories-first -la $*
+doskey lt=lsd --tree $*
+doskey ltd=lsd --tree --depth $*
 
 doskey md=mkdir ""$*"" $T cd ""$*""
 
