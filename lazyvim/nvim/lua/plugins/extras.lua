96dcc905c9673b4edfe296455bfc45854d352195
Diego Pahua Silvan <88301232+DiegPS@users.noreply.github.com>
2024-05-22 15:27:01 -0600

    feat: delete all

---
+++ lazyvim/nvim/lua/plugins/extras.lua
@@ -0,0 +1,14 @@
+return {{
+    import = "lazyvim.plugins.extras.lang.typescript"
+}, {
+    import = "lazyvim.plugins.extras.lang.markdown"
+}, {
+    import = "lazyvim.plugins.extras.lang.json"
+}, {
+    import = "lazyvim.plugins.extras.editor.illuminate"
+}, {
+    import = "lazyvim.plugins.extras.lang.tailwind"
+}, {
+    import = "lazyvim.plugins.extras.util.mini-hipatterns"
+}
+}
--- lazyvim/nvim/lua/plugins/modicator.lua
+++ lazyvim/nvim/lua/plugins/modicator.lua
@@ -1,14 +1,8 @@
 return {
-  -- Changes color of the cursor's line number based on the current vim mode
   {
     "mawkler/modicator.nvim",
     dependencies = "folke/tokyonight.nvim",
     event = "VeryLazy",
-    --[[ init = function()
-      vim.o.cursorline = true
-      vim.o.number = true
-      vim.o.termguicolors = true
-    end, ]]
     opts = {},
   },
 }
