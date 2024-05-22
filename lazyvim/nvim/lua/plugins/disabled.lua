a73c5300b4572cd47bde5f285c378223178323aa
Diego Pahua Silvan <88301232+DiegPS@users.noreply.github.com>
2024-05-22 12:21:16 -0600

    feat: new disabled lua

--- lazyvim/nvim/lua/plugins/disabled.lua
+++ lazyvim/nvim/lua/plugins/disabled.lua
@@ -3,4 +3,7 @@ return {
   { "nvim-lualine/lualine.nvim", enabled = false },
   { "akinsho/bufferline.nvim", enabled = false },
   { "nvimdev/dashboard-nvim", enabled = false },
+  { "folke/persistence.nvim", enabled = false },
+  { "folke/todo-comments.nvim", enabled = false },
+  { "catppuccin", enabled = false },
 }
